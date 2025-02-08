12aa2657a37c37862aaaff5930896b37bf262696
Joseph Massa <jmassa@umich.edu>
2025-02-08 13:42:09 -0500

    Basic network connection for client and server written.

--- src/network_manager.cpp
+++ src/network_manager.cpp
@@ -54,5 +54,68 @@ bool NetworkManager::initialize_server() {
         return false;
     }
 
+    // start listening for new client connections
+    if (listen(_socket, 6) == -1) {
+        perror("listen failed");
+        return false;
+    }
+
+    _client_sockets.reserve(6);
+
     return true;
+}
+
+int NetworkManager::accept_client() {
+    struct sockaddr *their_addr;
+    socklen_t sin_size = sizeof(their_addr);
+    int new_socket = accept(_socket, (struct sockaddr *)&their_addr, &sin_size);
+    if (new_socket == -1) {
+        perror("accept");
+        return -1;
+    }
+
+    _client_sockets.push_back(new_socket);
+    return new_socket;
+}
+
+int NetworkManager::connect_to_server(const char* ip_address) {
+    int numbytes;
+    struct addrinfo hints, *servinfo;
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+
+    int rv;
+    if ((rv = getaddrinfo(ip_address, PORT, &hints, &servinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+        return -1;
+    }
+
+    // loop through all the results and connect to the first we can
+    struct addrinfo * p;
+    for(p = servinfo; p != NULL; p = p->ai_next) {
+        if ((_socket = socket(p->ai_family, p->ai_socktype,
+                p->ai_protocol)) == -1) {
+            perror("client: socket");
+            continue;
+        }
+
+        if (connect(_socket, p->ai_addr, p->ai_addrlen) == -1) {
+            close(_socket);
+            perror("client: connect");
+            continue;
+        }
+
+        break;
+    }
+
+    if (p == NULL) {
+        fprintf(stderr, "client: failed to connect\n");
+        return -1;
+    }
+
+    freeaddrinfo(servinfo); // all done with this structure
+
+    return _socket;
 }
